7a779a333d3c7df65a30fc5ef5f2e7039c821ac8
Vinson Mach <mach.vinson@gmail.com>
2022-07-04 22:49:29 -0500

    Added various classes and functions
    Added various classes and functions:
    Card Class
    Deck Class
    Hand Class
    Chips Class
    takeBet function
    hit function
    hitOrStand function
    showSome function
    showAll function
    playerBusts function
    playerWin function
    dealerBusts function
    dealerWin function
    push function

--- BlackJack/BlackJack/BlackJack.py
+++ BlackJack/BlackJack/BlackJack.py
@@ -54,85 +54,102 @@ class Hand:
         self.aces = 0
 
     def addCard(self, card):
-        pass
+        self.cards.append(card)
+        self.value += values[card.rank]
+
+        if card.rank == 'Ace':
+            self.aces += 1
 
     def adjustForAce(self):
-        pass
+        while self.value > 21 and self.aces:
+            self.value -= 10
+            self.aces -= 1
 
-# Player class handles adding and removing cards
-class Player:
-    def __init__(self, name):
-        self.name = name                                        # assign name
-        self.allCards = []                                      # initialize player card deck
+class Chips:
+    def __init__(self, balance = 100):
+        self.balance = 100
+        self.bet = 0
 
-    def removeOne(self):
-        return self.allCards.pop(0)                             # remove player first card
+    def winBet(self):
+        self.balance += balance.bet
 
-    def addCards(self, newCards):
-        if type(newCards) == type([]):
-            self.allCards.extend(newCards)                      # extend card from player deck
+    def lostBet(self):
+        self.balance -= balance.bet
+
+def takeBet(chips):
+    while True:
+        try:
+            chips.bet = int(input("How many chips would you like to bet? "))
+        except:
+            print("Error: User input is not an integer")
         else:
-            self.allCards.append(newCards)                      # add card to player deck
+            if chips.bet > chips.balance:
+                print("Error: Invalid player chip balance, you have: {}".format(chips.balance))
+            else:
+                break
 
-    def __str__(self):
-        print('Player {} has {} cards.'.format(self.name, len(self.allCards)))  # print number of cards player has
+def hit(deck, hand):
+    singleCard = deck.deal()
+    hand.addCard(singleCard)
+    hand.adjustForAce()
+
+def hitOrStand(deck, hand):
+    global playing
+
+    while True:
+        x = input("Hit or Stand? Enter H or S: ")
+        if x[0].upper() == 'H':
+            hit(deck, hand)
+        elif x[0].upper() == 'S':
+            print("Player stand, Dealer's Turn.")
+            playing = False
+        else:
+            print("Error: Invalid input, please enter H or S")
+            continue
+        break
+
+def showSome(player, dealer):
+    print("\nDealer's Hand: ")
+    print("First card is hidden.")
+    print(dealer.cards[1])
+
+    print("\nPlayer's Hand: ")
+    for card in player.cards:
+        print(card)
+
+def showAll(player, dealer):
+    print("\nDealer's Hand: ")
+    for card in dealer.cards:
+        print(card)
+
+    print("Value of Dealer's Hand: {}".format(dealer.value))
+    
+    print("\nPlayer's Hand: ")
+    for card in player.cards:
+        print(card)
+    print("Value of Player's Hand: {}".format(player.value))
+
+def playerBusts(player, dealer, chips):
+    print("Player Busts!")
+    chips.loseBet()
+
+def playerWin(player, dealer, chips):
+    print("Player Wins!")
+    chips.winBet()
+
+def dealerBusts(player, dealer, chips):
+    print("Dealer Busts!")
+    chips.winBet()
+
+def dealerWin(player, dealer, chips):
+    print("Dealer Win!")
+    chips.loseBet()
+
+def push(player, dealer):
+    print("Dealer and Player tie!")
 
 # Main Function will handle game logic, intro and end
 def main():
-    p1 = Player("One")                                          # initialize player 1
-    p2 = Player("Two")                                          # initialize player 2
-    newDeck = Deck()                                            # initialize new deck
-    newDeck.shuffle()                                           # shuffle new deck
-
-    for x in range(26):
-        p1.addCards(newDeck.dealOne())                          # deal half the deck to player 1
-        p2.addCards(newDeck.dealOne())                          # deal the other half of the deck to player 2
-
-    roundNum = 0                                                # initialize round counter
-    gameOn = True                                               # continue game token
-    atWar = True                                                # war calculation token
-
-    while gameOn:
-        roundNum += 1                                           # add 1 to round counter
-        print("Round {}".format(roundNum))                      # print round number
-
-        if len(p1.allCards) == 0:
-            print('Player One is out of cards, Player Two wins.')   # print player 2 win statement
-            gameOn = False                                          # end game
-            break
-        if len(p2.allCards) == 0:
-            print('Player Two is out of cards, Player One wins.')   # print player 1 win statement
-            gameOn = False                                          # end game
-            break
-
-        p1Cards = []                                            # initialize player 1 cards
-        p1Cards.append(p1.removeOne())                          # add current player 1 card to player 1 deck
-        p2Cards = []                                            # initialize player 2 cards
-        p2Cards.append(p2.removeOne())                          # add current player 2 card to player 2 deck
-
-        while atWar:
-            if p1Cards[-1].value > p2Cards[-1].value:           # if last card in player 1 deck is greater than player 2 last card
-                p1.addCards(p1Cards)                            # add current player 1 card to player 1 deck
-                p1.addCards(p2Cards)                            # ass current player 2 card to player 1 deck
-                atWar = False                                   # end war comparison 
-            elif p1Cards[-1].value < p2Cards[-1].value:         # if last card in player 1 deck is lesser than player 2 last card
-                p2.addCards(p1Cards)                            # add current player 1 card to player 2 deck
-                p2.addCards(p2Cards)                            # add current player 2 card to player 2 deck
-                atWar = False                                   # end war comparison
-            else:
-                print("War is taking place.")                   # print war occuring statement
-                if len(p1.allCards) < 5:
-                    print("Player 1 is unable to continue, Player 2 wins.") # print player 2 win statement
-                    gameOn = False                                          # end game
-                    break
-                elif len(p2.allCards) < 5:
-                    print("Player 2 is unable to continue, Player 1 wins.") # print player 1 win statement
-                    gameOn = False                                          # end game
-                    break
-                else: 
-                    for num in range(5):
-                        p1Cards.append(p1.removeOne())          # add more cards to player 1
-                        p2Cards.append(p2.removeOne())          # add more cards to player 2
 
 if __name__ == "__main__":
     main()
