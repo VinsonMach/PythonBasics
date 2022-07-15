c4aebe40175255a9cfb6b739758528217e7c3690
Vinson Mach <mach.vinson@gmail.com>
2022-07-15 17:01:18 -0500

    Initial FizzBuzz Commit
    Initial commit of the FizzBuzz program

---
+++ FizzBuzz/FizzBuzz/FizzBuzz.py
@@ -0,0 +1,5 @@
+# Vinson Mach 7/15/2022
+# This program will print numbers from 1 to 100
+# For multiples of 3, the program will print "Fizz" and "Buzz" for multiples of 5
+# For multiples of both 3 and 5, the program will print "FizzBuzz"
+
