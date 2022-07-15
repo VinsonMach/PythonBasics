96e36116bfb26bbcb7ebd015a267050991cdff53
Vinson Mach <mach.vinson@gmail.com>
2022-07-15 17:06:31 -0500

    Completed FizzBuzz program

--- FizzBuzz/FizzBuzz/FizzBuzz.py
+++ FizzBuzz/FizzBuzz/FizzBuzz.py
@@ -3,3 +3,15 @@
 # For multiples of 3, the program will print "Fizz" and "Buzz" for multiples of 5
 # For multiples of both 3 and 5, the program will print "FizzBuzz"
 
+if __name__ == '__main__':
+
+    for i in range(1, 101):
+        if (i % 3 == 0):
+            if (i % 5 == 0):
+                print("FizzBuzz")
+            else:
+                print("Fizz")
+        elif (i % 5 == 0):
+            print("Buzz")
+        else:
+            print(i)
